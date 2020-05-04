349b1fbecd655e3b8849e54a5c14c2d2b383e6d0
Yeo, EunSeong <e7838752@gmail.com>
2020-05-04 10:00:13 +0900

    10:00 commit

--- skhu-yeo/public/구현시험 문제/mid5.js
+++ skhu-yeo/public/구현시험 문제/mid5.js
@@ -7,10 +7,11 @@
 
 let a = []; 
 
-for (let i = 0; i < 10 ; i++) {
+for (let i = 0; i < 100 ; i++) {
         a[i] = Math.floor(Math.random() * (100) + 1);
     }
+let even_filter = a.filter((e) => e % 2 === 0);
+let result = even_filter.reduce((a, b) => a + b);
 
-console.log(a);
+console.log(result);
 
-a.filter()
